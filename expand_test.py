4566038ebb0c2887ca2f91987163eda54c94f90d
Pato <boatengpato.pb@gmail.com>
2021-07-01 22:46:45 -0400

    Updated the test for expand

--- expand_test.py
+++ expand_test.py
@@ -15,6 +15,10 @@ class TestExpandedForm(unittest.TestCase):
         self.assertEqual(expanded_form('10000g'), '10000g is not a number')
         self.assertEqual(expanded_form('abcd'), 'abcd is not a number')
 
+    def test_digits_in_words(self):
+        self.assertEqual(expanded_form('thirty four'), '30 + 4')
+        self.assertEqual(expanded_form('two million four hundred and eight'), '2000000 + 400 + 8')
+
     def __str__(self):
         return 'TestExpandedForm'
 
